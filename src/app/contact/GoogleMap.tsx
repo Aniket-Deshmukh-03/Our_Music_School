1b853f3a61dc3f9ff9d15e12996f29b5bb93d32b
Aniket Deshmukh <aniketsd1998@gmail.com>
2025-02-15 15:11:10 +0530

    map

--- src/app/contact/GoogleMap.tsx
+++ src/app/contact/GoogleMap.tsx
@@ -3,14 +3,15 @@ import React from "react";
 export function GoogleMap() {
   return (
     <div className="w-full h-64 bg-gray-200 rounded-lg mb-6 flex items-center justify-center">
-      <iframe
+      {/* <iframe
         title="Google Map"
         className="w-full h-full rounded-lg"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4917377.387928487!2d72.77524473106466!3d19.75147980425143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf708432c217%3A0xd37fd8dcb7e47430!2sMaharashtra%2C%20India!5e0!3m2!1sen!2sus!4v1674497601023!5m2!1sen!2sus"
+        src=" "
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      ></iframe> */}
+      <h2 className="text-black"> Google Map Location</h2>
     </div>
   );
 }
